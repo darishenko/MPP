274e62850db5bb36de21808878b9770ca8f55f60
darishenko <dar1shenko@yandex.com>
2022-10-02 21:45:45 +0300

    Implement MethodTracer

---
+++ Tracer/TracerLibrary/Logic/MethodTracer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using TracerLibrary.Model;
+
+namespace TracerLibrary.Logic
+{
+
+    public class MethodTracer
+    {
+        public Stopwatch Stopwatch { get; } = new();
+        public ConcurrentStack<MethodInfo> Methods { get; } = new();
+
+        public void AddMethod(MethodInfo methodInfo)
+        {
+            Methods.Push(methodInfo);
+        }
+    }
+}
