7215c320929fd179404d4c7f8a4da04c1e3bbf72
darishenko <dar1shenko@yandex.com>
2022-10-02 21:55:39 +0300

    Add xml serialization

---
+++ Tracer/TracerLibrary/TracerLibrary/Serialization/XmlTraceSerializer.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+using TracerLibrary.Model;
+using TracerLibrary.Serialization.Interface;
+
+namespace TracerLibrary.Serialization.Serializer
+{
+
+    public class XmlTraceSerializer : ITraceSerializer
+    {
+        public string Serialize(TraceResult traceResult)
+        {
+            using var memoryStream = new MemoryStream();
+            new XmlSerializer(typeof(TraceResult)).Serialize(memoryStream, traceResult);
+            memoryStream.Position = 0;
+            return new StreamReader(memoryStream).ReadToEnd();
+        }
+
+        public TraceResult? Deserialize(string traceResult)
+        {
+            using var reader = new StringReader(traceResult);
+            return (TraceResult) new XmlSerializer(typeof(TraceResult)).Deserialize(reader)!;
+        }
+
+        public string GetFileExtension()
+        {
+            return ".xml";
+        }
+    }
+}
