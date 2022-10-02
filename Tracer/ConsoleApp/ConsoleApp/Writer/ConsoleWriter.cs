7775b83e629ad3bf9389fd85ef2b2f97f9879a60
darishenko <dar1shenko@yandex.com>
2022-10-02 22:09:50 +0300

    add Console and File Writer

---
+++ Tracer/ConsoleApp/ConsoleApp/Writer/ConsoleWriter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TracerLibrary.Model;
+using TracerLibrary.Serialization.Interface;
+
+namespace Application.Writer
+{
+
+    public class ConsoleWriter : IWriter
+    {
+        private ITraceSerializer _traceSerializer;
+
+        public ConsoleWriter(ITraceSerializer traceSerializer)
+        {
+            _traceSerializer = traceSerializer;
+        }
+
+        public void Print(TraceResult traceResult)
+        {
+            var result = traceResult.ToString();
+            if (_traceSerializer != null)
+            {
+                result = _traceSerializer.Serialize(traceResult);
+            }
+
+            Console.WriteLine(result);
+        }
+
+        public void SetTraceSerializer(ITraceSerializer traceSerializer)
+        {
+            _traceSerializer = traceSerializer;
+        }
+    }
+}
---
+++ Tracer/ConsoleApp/ConsoleApp/Writer/FileWriter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using TracerLibrary.Model;
+using TracerLibrary.Serialization.Interface;
+
+namespace Application.Writer
+{
+
+    public class FileWriter : IWriter
+    {
+        private ITraceSerializer _traceSerializer { get; set; }
+        private String _fileName;
+
+        public FileWriter(ITraceSerializer traceSerializer, String fileName)
+        {
+            _traceSerializer = traceSerializer;
+            _fileName = fileName;
+        }
+
+        public void Print(TraceResult traceResult)
+        {
+            String fileName = _fileName + _traceSerializer.GetFileExtension();
+            var result = traceResult.ToString();
+            if (_traceSerializer != null)
+            {
+                result = _traceSerializer.Serialize(traceResult);
+            }
+
+            File.WriteAllText(fileName, result);
+        }
+
+        public void SetTraceSerializer(ITraceSerializer traceSerializer)
+        {
+            _traceSerializer = traceSerializer;
+        }
+    }
+}
