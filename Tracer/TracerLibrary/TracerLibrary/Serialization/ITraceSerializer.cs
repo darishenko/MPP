e1a4390b1254d5d52b5b026227bb983ba611c51c
darishenko <dar1shenko@yandex.com>
2022-10-02 21:54:17 +0300

    Add serialization interface

---
+++ Tracer/TracerLibrary/TracerLibrary/Serialization/ITraceSerializer.cs
@@ -0,0 +1,12 @@
+﻿using TracerLibrary.Model;
+
+namespace TracerLibrary.Serialization.Interface
+{
+
+    public interface ITraceSerializer
+    {
+        string Serialize(TraceResult traceResult);
+        TraceResult? Deserialize(string traceResult);
+        string GetFileExtension();
+    }
+}
