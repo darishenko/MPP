97b1eafe057c8807b9d9f0652a972dc613643120
darishenko <dar1shenko@yandex.com>
2022-10-02 22:09:17 +0300

    Add interface to output serialization traceResult

---
+++ Tracer/ConsoleApp/ConsoleApp/Writer/IWriter.cs
@@ -0,0 +1,13 @@
+﻿using TracerLibrary.Model;
+using TracerLibrary.Serialization.Interface;
+
+namespace Application.Writer
+{
+
+    public interface IWriter
+    {
+        void Print(TraceResult traceResult);
+
+        void SetTraceSerializer(ITraceSerializer traceSerializer);
+    }
+}
