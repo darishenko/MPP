f2fb80b2361498df3b094d3f0af80e6882cb308a
darishenko <dar1shenko@yandex.com>
2022-10-02 21:46:32 +0300

    Implement ThreadTracer

---
+++ Tracer/TracerLibrary/Logic/ThreadTracer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Linq;
+using TracerLibrary.Model;
+
+namespace TracerLibrary.Logic
+{
+
+    public class ThreadTracer
+    {
+        public ConcurrentStack<MethodTracer> MethodsTraces { get; init; } = new();
+        public ConcurrentStack<MethodInfo> MethodsInfo { get; init; } = new();
+        public MethodTracer CurrentMethodTracer { get; set; } = null!;
+
+        public void StartTrace()
+        {
+            if (CurrentMethodTracer is not null)
+            {
+                MethodsTraces.Push(CurrentMethodTracer);
+            }
+
+            CurrentMethodTracer = new MethodTracer();
+            CurrentMethodTracer.Stopwatch.Start();
+        }
+
+        public void StopTrace(StackFrame stackFrame)
+        {
+            CurrentMethodTracer.Stopwatch.Stop();
+            var methodInfo = new MethodInfo()
+            {
+                Name = stackFrame.GetMethod()?.Name,
+                Class = stackFrame.GetMethod()?.ReflectedType?.Name,
+                ElapsedMills = CurrentMethodTracer.Stopwatch.ElapsedMilliseconds,
+                Methods = CurrentMethodTracer.Methods.ToList()
+            };
+
+            if (MethodsTraces.IsEmpty)
+            {
+                MethodsInfo.Push(methodInfo);
+                CurrentMethodTracer = null;
+            }
+            else
+            {
+                if (MethodsTraces.TryPop(out var methodTrace))
+                {
+                    CurrentMethodTracer = methodTrace;
+                    CurrentMethodTracer.AddMethod(methodInfo);
+                }
+            }
+        }
+    }
+}
