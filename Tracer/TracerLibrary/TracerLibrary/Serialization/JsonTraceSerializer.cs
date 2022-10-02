9d9a2a4e4f10c9df975e5f439b96bf8b25d06a92
darishenko <dar1shenko@yandex.com>
2022-10-02 21:56:18 +0300

    Add json serialization

---
+++ Tracer/TracerLibrary/TracerLibrary/Serialization/JsonTraceSerializer.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using TracerLibrary.Model;
+using TracerLibrary.Serialization.Interface;
+
+namespace TracerLibrary.Serialization.Serializer
+{
+
+    public class JsonTraceSerializer : ITraceSerializer
+    {
+        public string Serialize(TraceResult traceResult)
+        {
+            return JsonConvert.SerializeObject(traceResult, Formatting.Indented);
+        }
+
+        public TraceResult Deserialize(string traceResult)
+        {
+            return JsonConvert.DeserializeObject<TraceResult>(traceResult);
+        }
+
+        public string GetFileExtension()
+        {
+            return ".json";
+        }
+    }
+}
