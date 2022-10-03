d1b45981979c5b6cb94b0bf28b88ab7da71fe617
darishenko <dar1shenko@yandex.com>
2022-10-03 23:24:45 +0300

    add unit tests

---
+++ Tracer/TracerLibrary.Tests/TracerLibrary.Tests/ThreadTracerTests.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using TracerLibrary.Logic;
+using Xunit;
+
+namespace TracerLibrary.Tests
+{
+    public class ThreadTracerTests
+    {
+        [Fact]
+        public void StartTrace_CurrentMethodTraceIsNotNull_AddCurrentMethodTraceToMethodsTraces()
+        {
+            var currentMethodTracer = new MethodTracer();
+            var threadTracer = new ThreadTracer
+            {
+                CurrentMethodTracer = currentMethodTracer
+            };
+
+            threadTracer.StartTrace();
+
+            Assert.Contains(currentMethodTracer, threadTracer.MethodsTraces);
+        }
+
+        [Fact]
+        public void StartTrace_CurrentMethodTraceIsNotNull_NewCurrentMethodTraceInitializes()
+        {
+            var currentMethodTracer = new MethodTracer();
+            var threadTracer = new ThreadTracer
+            {
+                CurrentMethodTracer = currentMethodTracer
+            };
+
+            threadTracer.StartTrace();
+
+            Assert.NotEqual(currentMethodTracer, threadTracer.CurrentMethodTracer);
+        }
+
+        [Fact]
+        public void StartTrace_CurrentMethodTraceIsNull_CurrentMethodTraceInitializes()
+        {
+            var threadTracer = new ThreadTracer
+            {
+                CurrentMethodTracer = null
+            };
+
+            threadTracer.StartTrace();
+
+            Assert.NotNull(threadTracer.CurrentMethodTracer);
+        }
+
+        [Fact]
+        public void StartTrace_RunningStopwatch_StopwatchRun()
+        {
+            var threadTracer = new ThreadTracer
+            {
+                CurrentMethodTracer = null
+            };
+
+            threadTracer.StartTrace();
+
+            Assert.True(threadTracer.CurrentMethodTracer.Stopwatch.IsRunning);
+        }
+
+        [Fact]
+        public void StopTrace_StoppingStopwatch_StopwatchStopped()
+        {
+            var threadTracer = new ThreadTracer
+            {
+                CurrentMethodTracer = null
+            };
+            threadTracer.StartTrace();
+
+            var currentMethodTracer = threadTracer.CurrentMethodTracer;
+
+            threadTracer.StopTrace(new StackTrace().GetFrame(1));
+
+            Assert.False(currentMethodTracer.Stopwatch.IsRunning);
+        }
+
+        [Fact]
+        public void StopTrace_MethodsTracesIsEmpty_AddedMethodInfoToMethodsInfo()
+        {
+            var threadTracer = new ThreadTracer
+            {
+                CurrentMethodTracer = null
+            };
+            threadTracer.StartTrace();
+
+            threadTracer.StopTrace(new StackTrace().GetFrame(1));
+
+            Assert.NotEmpty(threadTracer.MethodsInfo);
+        }
+
+        [Fact]
+        public void StopTrace_MethodsTracesIsNotEmpty_AddedMethodInfoToCurrentMethodTracer()
+        {
+            var threadTracer = new ThreadTracer
+            {
+                CurrentMethodTracer = null,
+                MethodsTraces = new ConcurrentStack<MethodTracer>(new[]
+                {
+                    new MethodTracer()
+                })
+            };
+
+            threadTracer.StartTrace();
+
+            threadTracer.StopTrace(new StackTrace().GetFrame(1));
+
+            Assert.NotEmpty(threadTracer.CurrentMethodTracer.Methods);
+        }
+    }
+}
---
+++ Tracer/TracerLibrary.Tests/TracerLibrary.Tests/TracerTests.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using TracerLibrary.Logic;
+using Xunit;
+
+namespace TracerLibrary.Tests
+{
+    public class TracerTests
+    {
+        [Fact]
+        public void GetTraceResult_ThreadsTraceIsEmpty_ReturnedTraceResultWithEmptyThreadInfos()
+        {
+            var tracer = new MainTracer();
+
+            var result = tracer.GetTraceResult();
+            Assert.Empty(result.ThreadsInfo);
+        }
+
+        [Fact]
+        public void GetTraceResult_OneMethodInOneThread_ReturnedTraceResultWithOneMethodInOneThread()
+        {
+            var tracer = new MainTracer();
+            var testClass = new TestClass(tracer);
+            testClass.TestMethod1();
+
+            var result = tracer.GetTraceResult();
+            Assert.Single(result.ThreadsInfo);
+            Assert.Single(result.ThreadsInfo[0].Methods);
+        }
+
+        [Fact]
+        public void GetTraceResult_OneMethodInOneThread_ReturnedTraceResultWithCorrectElapsedMilliseconds()
+        {
+            var tracer = new MainTracer();
+            var testClass = new TestClass(tracer);
+            testClass.TestMethod1();
+
+            var result = tracer.GetTraceResult();
+            Assert.True(result.ThreadsInfo[0].Methods[0].ElapsedMills >= 10);
+            Assert.True(result.ThreadsInfo[0].TotalElapsedMilliseconds >=
+                        result.ThreadsInfo[0].Methods[0].ElapsedMills);
+        }
+
+        [Fact]
+        public void GetTraceResult_OneMethodInOneThread_ReturnedTraceResultWithCorrectNames()
+        {
+            var tracer = new MainTracer();
+            var testClass = new TestClass(tracer);
+            testClass.TestMethod1();
+
+            var result = tracer.GetTraceResult();
+            Assert.Equal(nameof(testClass.TestMethod1), result.ThreadsInfo[0].Methods[0].Name);
+            Assert.Equal(nameof(TestClass), result.ThreadsInfo[0].Methods[0].Class);
+        }
+
+        [Fact]
+        public void GetTraceResult_NestedMethodsInOneThread_ReturnedTraceResultWithCorrectNesting()
+        {
+            var tracer = new MainTracer();
+            var testClass = new TestClass(tracer);
+            testClass.TestMethod2();
+
+            var result = tracer.GetTraceResult();
+            Assert.Single(result.ThreadsInfo);
+            Assert.Single(result.ThreadsInfo[0].Methods);
+            Assert.Single(result.ThreadsInfo[0].Methods[0].Methods);
+        }
+
+        [Fact]
+        public void GetTraceResult_NestedMethodsInOneThread_ReturnedTraceResultWithCorrectNames()
+        {
+            var tracer = new MainTracer();
+            var testClass = new TestClass(tracer);
+            testClass.TestMethod2();
+
+            var result = tracer.GetTraceResult();
+            Assert.Equal(nameof(testClass.TestMethod2), result.ThreadsInfo[0].Methods[0].Name);
+            Assert.Equal(nameof(TestClass), result.ThreadsInfo[0].Methods[0].Class);
+            Assert.Equal(nameof(testClass.TestMethod1), result.ThreadsInfo[0].Methods[0].Methods[0].Name);
+            Assert.Equal(nameof(TestClass), result.ThreadsInfo[0].Methods[0].Methods[0].Class);
+        }
+
+        [Fact]
+        public void GetTraceResult_NestedMethodsInOneThread_ReturnedTraceResultWithCorrectElapsedMilliseconds()
+        {
+            var tracer = new MainTracer();
+            var testClass = new TestClass(tracer);
+            testClass.TestMethod2();
+
+            var result = tracer.GetTraceResult();
+            Assert.True(result.ThreadsInfo[0].Methods[0].ElapsedMills >= 20);
+            Assert.True(result.ThreadsInfo[0].TotalElapsedMilliseconds >=
+                        result.ThreadsInfo[0].Methods[0].ElapsedMills);
+            Assert.True(result.ThreadsInfo[0].Methods[0].Methods[0].ElapsedMills >= 10);
+            Assert.True(result.ThreadsInfo[0].Methods[0].ElapsedMills >=
+                        result.ThreadsInfo[0].Methods[0].Methods[0].ElapsedMills);
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        public void GetTraceResult_ManyThreads_ReturnedTraceResultWithCorrectThreadsCount(int countOfThreads)
+        {
+            var tracer = new MainTracer();
+            var testClass = new TestClass(tracer);
+
+            var events = new List<WaitHandle>();
+
+            for (var i = 0; i < countOfThreads; i++)
+            {
+                var resetEvent = new ManualResetEvent(false);
+                ThreadPool.QueueUserWorkItem(
+                    _ =>
+                    {
+                        testClass.TestMethod1();
+                        resetEvent.Set();
+                    });
+                events.Add(resetEvent);
+            }
+
+            WaitHandle.WaitAll(events.ToArray());
+
+            var result = tracer.GetTraceResult();
+            Assert.Equal(countOfThreads, result.ThreadsInfo.Count);
+        }
+
+        private class TestClass
+        {
+            private readonly MainTracer _tracer;
+
+            public TestClass(MainTracer tracer)
+            {
+                _tracer = tracer;
+            }
+
+            public void TestMethod1()
+            {
+                _tracer.StartTrace();
+                Thread.Sleep(10);
+                _tracer.StopTrace();
+            }
+
+            public void TestMethod2()
+            {
+                _tracer.StartTrace();
+                Thread.Sleep(20);
+                TestMethod1();
+                _tracer.StopTrace();
+            }
+        }
+    }
+}
