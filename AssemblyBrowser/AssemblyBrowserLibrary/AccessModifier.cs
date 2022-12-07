4b29804146da933ee011bbd1ab6607ab548f8f9c
darishenko <dar1shenko@yandex.com>
2022-12-07 03:05:56 +0300

    Add assembly browser implementation

---
+++ AssemblyBrowser/AssemblyBrowserLibrary/AccessModifier.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyBrowserLibrary
+{
+    public static class AccessModifier
+    {
+        public static string GetMethodAccessModifier(MethodInfo method)
+        {
+            var result = "";
+            if (method.IsPublic) result += "public";
+            else if (method.IsPrivate) result += "private";
+            else if (method.IsFamily) result += "protected";
+            else if (method.IsAssembly) result += "internal";
+            else if (method.IsFamilyOrAssembly) result += "protected internal";
+            
+            if (method.IsStatic) result += " static";
+            else if (method.IsAbstract) result += " abstract";
+            else if (method.IsVirtual) result += " virtual";
+            else if (method.GetBaseDefinition() != method) result += " override";
+            
+            return result;
+        }
+
+        public static string GetClassAccessModifier(Type type)
+        {
+            var result = "";
+            
+            if (type.IsPublic) result += "public";
+            else if (type.IsNestedPrivate) result += "private";
+            else if (type.IsNestedFamily) result += "protected";
+            else if (type.IsNestedAssembly) result += "internal";
+            else if (type.IsNestedFamANDAssem) result += "protected internal";
+            else if (type.IsNotPublic) result += "private";
+            
+            if (type.IsAbstract && type.IsSealed) result += " static";
+            else if (type.IsAbstract && !type.IsInterface) result += " abstract";
+            
+            if (type.IsClass) result += " class";
+            if (type.IsEnum) result += " enum";
+            if (type.IsInterface) result += " interface";
+            if (type.IsValueType && type.IsPrimitive) result += " struct";
+            
+            return result;
+        }
+
+        public static string GetFieldAccessModifier(FieldInfo field)
+        {
+            if (field == null) return "";
+
+            var result = "";
+            if (field.IsPublic) result += "public";
+            else if (field.IsPrivate) result += "private";
+            else if (field.IsFamily) result += "protected";
+            else if (field.IsAssembly) result += "internal";
+            else if (field.IsFamilyOrAssembly) result += "protected internal";
+            
+            if (field.IsStatic) result += " static";
+            
+            return result;
+        }
+
+        public static string GetPropertyAccessModifier(PropertyInfo prop)
+        {
+            if (prop == null) return "";
+            
+            var result = "";
+            
+            if (prop.CanRead) result += "public";
+            else result += "private ";
+            
+            result += " get; ";
+            
+            if (prop.CanWrite) result += "public";
+            else result += "private";
+            
+            result += " set;";
+            
+            return result;
+        }
+    }
+}
---
+++ AssemblyBrowser/AssemblyBrowserLibrary/AssemblyBrowser.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace AssemblyBrowserLibrary
+{
+    public static class AssemblyBrowser
+    {
+        public static AssemblyInformation GetAssemblyInformation(string path)
+        {
+            var assembly = Assembly.LoadFrom(path);
+            var assemblyInformation = new AssemblyInformation(assembly.GetName().Name);
+            
+            var namespaces = assembly.GetTypes().Select(type => type.Namespace).Distinct().ToList()
+                .Where(nameSpace => nameSpace != null).ToList();
+            
+            namespaces.ForEach(nameSpace =>
+            {
+                var namespaceInformation = new NamespaceInformation(nameSpace);
+                assemblyInformation.Namespaces.Add(namespaceInformation);
+
+                var classes = assembly.GetTypes()
+                    .Where(type => type.IsClass && type.Namespace == nameSpace).ToList();
+
+                classes.ForEach(clazz =>
+                {
+                    var classInformation = new ClassInformation(clazz, clazz.Name);
+                    namespaceInformation.Classes.Add(classInformation);
+
+                    var fields = clazz.GetFields(
+                        BindingFlags.NonPublic | 
+                        BindingFlags.Public | 
+                        BindingFlags.Static |
+                        BindingFlags.Instance).ToList();
+                    
+                    var properties = clazz.GetProperties(
+                        BindingFlags.NonPublic | 
+                        BindingFlags.Public |
+                        BindingFlags.Static | 
+                        BindingFlags.Instance).ToList();
+                    
+                    var methods = clazz.GetMethods(
+                            BindingFlags.NonPublic | 
+                            BindingFlags.Public | 
+                            BindingFlags.Static |
+                            BindingFlags.Instance)
+                        .Where(method => !method.IsDefined(typeof(ExtensionAttribute))).ToList();
+
+                    fields.ForEach(field =>
+                        classInformation.Members.First(memberType => memberType.Name == "Fields")
+                            .AddField(field));
+                    
+                    properties.ForEach(
+                        property => classInformation.Members.First(memberType => memberType.Name == "Properties")
+                            .AddProperty(property));
+                    
+                    methods.ForEach(method =>
+                        classInformation.Members.First(memberType => memberType.Name == "Methods")
+                            .AddMethod(method, false));
+                    
+                });
+
+                classes.ForEach(clazz =>
+                {
+                    clazz.GetMethods().Where(method => method.IsDefined(typeof(ExtensionAttribute), false)).ToList()
+                        .ForEach(parameter =>
+                        {
+                            assemblyInformation.Namespaces.ToList().ForEach(nameSpace =>
+                                nameSpace.Classes.First(clazz =>
+                                        clazz.Name == parameter.GetParameters()[0].ParameterType.Name)
+                                    ?.Members.First(method => method.Name == "Methods")
+                                    .AddMethod(parameter, true));
+                        });
+                });
+                
+            });
+
+            return assemblyInformation;
+        }
+    }
+}
