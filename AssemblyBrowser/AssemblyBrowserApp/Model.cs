6b7da806cf57985ded72d4a1e4ab7a2c12f0a8b0
darishenko <dar1shenko@yandex.com>
2022-12-07 16:17:34 +0300

    update assembly browser

---
+++ AssemblyBrowser/AssemblyBrowserApp/Model.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using AssemblyBrowserLibrary;
+
+namespace AssemblyBrowser
+{
+    internal class Model : INotifyPropertyChanged
+    {
+        private ObservableCollection<AssemblyInformation> _assemblyData;
+        private string _assemblyFileName;
+
+        public string AssemblyFileName
+        {
+            get => _assemblyFileName;
+            set
+            {
+                _assemblyFileName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<AssemblyInformation> AssemblyData
+        {
+            get => _assemblyData;
+            set
+            {
+                _assemblyData = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ AssemblyBrowser/AssemblyBrowserApp/ViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using AssemblyBrowserLibrary;
+using Microsoft.Win32;
+
+namespace AssemblyBrowser
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        private readonly Model _model;
+        private ICommand _openFileCommand;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModel(Model model)
+        {
+            _model = model;
+        }
+
+        public string AssemblyFileName
+        {
+            get => _model.AssemblyFileName;
+            set
+            {
+                _model.AssemblyFileName = value;
+                AssemblyData = new ObservableCollection<AssemblyInformation>
+                {
+                    AssemblyBrowserLibrary.AssemblyBrowser.GetAssemblyInformation(_model.AssemblyFileName)
+                };
+                
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<AssemblyInformation> AssemblyData
+        {
+            get => _model.AssemblyData;
+            set
+            {
+                _model.AssemblyData = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand OpenFileCommand
+        {
+            get
+            {
+                return _openFileCommand ??= new Command(obj =>
+                {
+                    var dialog = new OpenFileDialog();
+                    if (dialog.ShowDialog() == true) 
+                        AssemblyFileName = dialog.FileName;
+                });
+            }
+        }
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
