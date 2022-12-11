b245e73b26b55e61e0f04ad44a9b408251c04624
darishenko <dar1shenko@yandex.com>
2022-12-11 23:21:47 +0300

    add async writing implementation

---
+++ TestsGenerator/TestsGenerator/AsyncFileWorker/AsyncFileWriter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TestsGenerator
+{
+    public class AsyncFileWriter
+    {
+        private string outputDirectory;
+
+        public AsyncFileWriter(string outPutDirectory)
+        {
+            this.outputDirectory = outPutDirectory;
+        }
+
+        public async Task Write(TestClassTemplate classTemplate)
+        {
+            if (classTemplate == null)
+            {
+                return;
+            }
+
+            string filePath = outputDirectory + "\\" + classTemplate.FileName;
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                await writer.WriteAsync(classTemplate.InnerText);
+            }
+        }
+    }
+}
