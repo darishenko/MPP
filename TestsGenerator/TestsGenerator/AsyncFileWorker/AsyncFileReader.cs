4e97f6852a3d265de3769ef184bb2ae68ff66d3c
darishenko <dar1shenko@yandex.com>
2022-12-11 23:24:04 +0300

    add async file reading implementation

---
+++ TestsGenerator/TestsGenerator/AsyncFileWorker/AsyncFileReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TestsGenerator
+{
+    public class AsyncFileReader
+    {
+        public async Task<string> Read(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
